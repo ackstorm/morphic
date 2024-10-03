ed4ed2b075328f63d226c09a05f330bead9652fe
Juan Carlos Moreno <juancarlos.moreno@ackstorm.com>
2024-10-03 13:19:09 +0200

    Update history-list.tsx

--- components/history-list.tsx
+++ components/history-list.tsx
@@ -14,12 +14,11 @@ export async function HistoryList() {
   const chats = await loadChats(userId)
 
   return (
-    <div className="ackstorm">userId: {userId}</div>
     <div className="flex flex-col flex-1 space-y-3 h-full">
       <div className="flex flex-col space-y-0.5 flex-1 overflow-y-auto">
         {!chats?.length ? (
           <div className="text-foreground/30 text-sm text-center py-4">
-            No search history
+            No search history for {userId}
           </div>
         ) : (
           chats?.map(
