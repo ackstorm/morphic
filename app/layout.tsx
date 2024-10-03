9907595f4ca2484b2f52bc5ed6f5d8d50bcb5fc5
Juan Carlos Moreno <juancarlos.moreno@ackstorm.com>
2024-10-03 17:17:20 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,12 +14,12 @@ const fontSans = FontSans({
   variable: '--font-sans'
 })
 
-const title = 'Morphic'
+const title = 'ACKsearch'
 const description =
-  'A fully open-source AI-powered answer engine with a generative UI.'
+  'A AI-powered answer engine with a generative UI.'
 
 export const metadata: Metadata = {
-  metadataBase: new URL('https://morphic.sh'),
+  metadataBase: new URL('https://ackstorm.com'),
   title,
   description,
   openGraph: {
