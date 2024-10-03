bd52827a76b9e0135a9d43b56776d29256ddf47c
Juan Carlos Moreno <juancarlos.moreno@ackstorm.com>
2024-10-03 12:23:37 +0200

    Update page.tsx

--- app/search/[id]/page.tsx
+++ app/search/[id]/page.tsx
@@ -9,7 +9,7 @@ export const maxDuration = 60
 async function getUserId() {
     'use server'
     const headersList = headers()
-    return headersList.get('X-Forwarded-Email') || 'anonymous'
+    return headersList.get('X-Forwarded-Email') || 'unknown'
 }
 
 export interface SearchPageProps {
