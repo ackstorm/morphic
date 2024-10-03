b686f22d973e8d6196dbc102e47a46ac2f0ba946
Juan Carlos Moreno <juancarlos.moreno@ackstorm.com>
2024-10-03 12:22:36 +0200

    Update page.tsx

--- app/search/[id]/page.tsx
+++ app/search/[id]/page.tsx
@@ -2,8 +2,15 @@ import { notFound, redirect } from 'next/navigation'
 import { Chat } from '@/components/chat'
 import { getChat } from '@/lib/actions/chat'
 import { AI } from '@/app/actions'
+import { headers } from 'next/headers'
 
 export const maxDuration = 60
+
+async function getUserId() {
+    'use server'
+    const headersList = headers()
+    return headersList.get('X-Forwarded-Email') || 'anonymous'
+}
 
 export interface SearchPageProps {
   params: {
@@ -12,14 +19,15 @@ export interface SearchPageProps {
 }
 
 export async function generateMetadata({ params }: SearchPageProps) {
-  const chat = await getChat(params.id, 'anonymous')
+  const userId = await getUserId()
+  const chat = await getChat(params.id, userId)
   return {
     title: chat?.title.toString().slice(0, 50) || 'Search'
   }
 }
 
 export default async function SearchPage({ params }: SearchPageProps) {
-  const userId = 'anonymous'
+  const userId = await getUserId()
   const chat = await getChat(params.id, userId)
 
   if (!chat) {
